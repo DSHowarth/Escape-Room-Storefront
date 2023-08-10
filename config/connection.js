f8c418bc159fa5946a2f8884dcd5b5350d840f71
moonjunsain <moonjunsain@gmail.com>
2023-08-10 15:06:48 -0700

    added comments for teammate

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,12 @@ const {Sequelize} = require('sequelize')
 require('dotenv').config()
 
 let sequelize;
-
+// depending on our deployment to heroku, we will use local server database or their jawsdb
 if(process.env.JAWSDB_URL){
     sequelize = new Sequelize(process.env.JAWSDB_URL)
 }else {
     sequelize = new Sequelize(
+        // make sure you have your configuration saved in .env
         process.env.DB_NAME,
         process.env.DB_USER,
         process.env.DB_PASSWORD,
