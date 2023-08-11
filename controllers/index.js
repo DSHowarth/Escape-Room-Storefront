e13ffa73aa19623c9fe0d7ffecda2b044d296b9d
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-08-11 15:58:42 -0500

    updated router path

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
-const router = require('express').Router()
-const renderRoute = require('./render/index')
-const apiRoute = require('./api/index')
+const router = require("express").Router();
+const renderRoute = require("./render/index");
+const apiRoute = require("./api/index");
+const homeRoutes = require("./render/home-render");
 
-router.use('/api', apiRoute)
-router.use('/', renderRoute)
+router.use("/api", apiRoute);
+router.use("/", renderRoute);
+router.use("/", homeRoutes);
 
-module.exports = router
+module.exports = router;
