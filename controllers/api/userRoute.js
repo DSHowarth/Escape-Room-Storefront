ba73f490bc2be02d90c54fd076260a72af93a159
moonjunsain <moonjunsain@gmail.com>
2023-08-11 12:58:06 -0700

    added a route for login in api/users

--- controllers/api/userRoute.js
+++ controllers/api/userRoute.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const {User} = require('../../model/index')
+const bcrypt = require('bcrypt')
 
 // end point with /api/users
 
@@ -38,7 +39,43 @@ router.post('/signup', async (req, res)=>{
 // user logs in
 router.post('/login', async (req, res) => {
     try{
-        
+        // checks if the body is valid
+        if(req.body.email && req.body.password){
+            // checks if the user email is valid
+            const user = User.findOne({
+                where: {
+                    email: req.body.email
+                }
+            })
+
+            // if user does not exist by the email
+            if(!user){
+                res.status(404).json({message: "Incorrect Email or password"})
+                return
+            }
+
+            // checks if their password matches
+            const isValid = await bcrypt.compare(req.body.password, user.password)
+
+            // if password does not match
+            if(!isValid){
+                res.status(404).json({message: "Incorrect Email or password"})
+                return
+            }
+
+            // set up a session status for logged in once the password matches
+            req.session.save(()=>{
+                // keeping track of user id
+                req.session.userId = user.id
+
+                // keeping track of login status
+                req.session.loggedIn = true
+
+                res.status(200).json({message: "Created a new user successfully", newUser})
+            })
+        }else {
+            res.status(404).json({message: 'invalid body passed'})
+        }
 
     }catch(error){
         res.status(500).json({message: "Error while logging in", err})
