ae621824b6a203a9e8fbc698657939745f98c708
Dash <dkshowarth@gmail.com>
2023-08-11 14:00:42 -0700

    target elements, add form for party size to handlebars

---
+++ public/js/reservation.js
@@ -0,0 +1,8 @@
+const resModal = document.getElementById('resModal')
+
+const modalBody = document.getElementById('modalBody')
+const confirm = document.getElementById('resConfirm')
+
+const login = document.getElementById('resLogin')
+const signUp = document.getElementById('resSignUp')
+
