c9654cfe4e1a0086f703dfb6cdf563a32786e653
moonjunsain <moonjunsain@gmail.com>
2023-08-11 11:23:00 -0700

    file setup for login and signup

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 // add your route like this
 // router.use('/routenmae', require('./filename'))
+router.use('/users', require('./userRoute'))
 
 module.exports = router
---
+++ controllers/api/userRoute.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+const {User} = require('../../model/index')
+
+// end point with /api/users
+
+// create a new user
+router.post('/signup', async (req, res)=>{
+    
+})
+
+module.exports = router
