dfbf478094fd819e910f4f8586e3482dcfe4768c
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-08-11 15:38:12 -0500

    auth and login routers

--- controllers/render/home-render.js
+++ controllers/render/home-render.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const { User, Reservation } = require(".../model");
-const withAuth = require(".../utils/auth.js");
+const { User, Reservation } = require("../../model");
+const withAuth = require("../../utils/auth.js");
 
 // --------------------------- GET All Router -------------------------------- //
 
@@ -26,6 +26,62 @@ router.get("/", async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+// --------------------------- GET One by ID Router -------------------------------- //
+
+router.get("/user/:id", async (req, res) => {
+  try {
+    const userdata = await User.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["name"],
+        },
+      ],
+    });
+    if (!userdata) {
+      res.status(404).json("No user found with this ID!");
+      return;
+    }
+    const user = userData.get({ plain: true });
+    res.render("user", {
+      ...user,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// --------------------------- Auth -------------------------------- //
+
+router.get("/profile", withAuth, async (req, res) => {
+  try {
+    // find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: User }],
+    });
+    const user = userData.get({ plain: true });
+    res.render("profile", {
+      ...user,
+      logged_in: true,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// --------------------------- Login -------------------------------- //
+
+router.get("/login", (req, res) => {
+  // if the user is already logged in, redirect the reuest to another route
+  if (req.session.logged_in) {
+    res.redirect("/profile");
+    return;
+  }
+  res.render("login");
 });
 
 module.exports = router;
