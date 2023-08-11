e083d9d9e03515e4651a174fd6b0f4efc40235a2
Dash <dkshowarth@gmail.com>
2023-08-11 16:28:40 -0700

    add form submission and party size validation to reservation modal, add corresponding js

--- public/js/reservation.js
+++ public/js/reservation.js
@@ -1,8 +1,25 @@
 const resModal = document.getElementById('resModal')
 
+
 const modalBody = document.getElementById('modalBody')
+const partyCountForm = document.getElementById('partyCountForm')
 const confirm = document.getElementById('resConfirm')
+
 
 const login = document.getElementById('resLogin')
 const signUp = document.getElementById('resSignUp')
 
+// Reservation submission via modal 'confirm' button
+partyCountForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    // if(!partyCountForm.checkValidity())
+    if ((myInput.value > 5)){
+      myInput.classList.remove('is-valid')
+      myInput.classList.add('is-invalid')
+    }
+    else {
+      myInput.classList.remove('is-invalid')
+      myInput.classList.add('is-valid')
+    }
+  }, false)
