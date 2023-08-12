bed62e3d9b2a0784aa2b7a02a869ebba7dc8fab3
moonjunsain <moonjunsain@gmail.com>
2023-08-11 19:34:06 -0700

    modified profile handlebars so that it dynamically generates date

---
+++ controllers/render/profile.js
@@ -0,0 +1,24 @@
+const router = require('express').Router()
+const {Reservation} = require('../../model')
+// end point with /profile
+
+// renders profile page
+router.get('/', async (req, res) => {
+    // must render logged in user's reservations
+    try{
+        const reservations = Reservation.findAll({
+            // finds the reservation where it matches user id
+            where: {
+                // since session saves user id
+                user_id: req.session.userId
+            }
+        })
+
+        
+
+    }catch(error){
+        res.status(500).json({message: "Server Error while getting reservation"})
+    }
+})
+
+module.exports = router
