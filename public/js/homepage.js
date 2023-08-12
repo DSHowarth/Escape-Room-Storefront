628b0b2899bea11900b70297de2224ff2171559a
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-08-12 18:28:09 -0500

    updated styling for the neon-button

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,19 +1,3 @@
-// const fileInput = document.querySelector("#player");
-
-// fileInput.addEventListener("change", async (event) => {
-//   const file = event.target.files[0];
-
-//   // Use Cloudinary's upload API to upload the file
-//   const response = await fetch("/upload", {
-//     method: "POST",
-//     body: file,
-//   });
-
-//   if (response.ok) {
-//   } else {
-//   }
-// });
-
 cloudinary.v2.uploader
   .upload(
     "https://res.cloudinary.com/dfecvj7s4/video/upload/v1691872048/escape-room-video_hdaybz.mp4",
