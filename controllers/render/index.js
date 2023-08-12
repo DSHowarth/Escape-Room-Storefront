675923c7c491d8dca16277d2cccdfff834e93735
Dash <dkshowarth@gmail.com>
2023-08-12 15:24:37 -0700

    add about route

--- controllers/render/index.js
+++ controllers/render/index.js
@@ -1,14 +1,16 @@
-const loginRoute = require('./login')
-const signUpRoute = require('./signup')
-const reserveRoute = require('./reservation')
-const homeRoutes = require('./homepage')
-const profileRoute = require('./profile')
-const router = require('express').Router()
+const loginRoute = require('./login');
+const signUpRoute = require('./signup');
+const reserveRoute = require('./reservation');
+const homeRoutes = require('./homepage');
+const profileRoute = require('./profile');
+const router = require('express').Router();
+const aboutRoute = require('./about');
 
-router.use('/login', loginRoute)
-router.use('/signup', signUpRoute)
-router.use('/reservations', reserveRoute)
-router.use('/profile', profileRoute)
+router.use('/login', loginRoute);
+router.use('/signup', signUpRoute);
+router.use('/reservations', reserveRoute);
+router.use('/profile', profileRoute);
+router.use('/about', aboutRoute);
 router.use("/", homeRoutes);
 
 module.exports = router;
