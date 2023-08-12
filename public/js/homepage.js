e04ed76edb8fb2577eac8cc5ca1c209e71d9e25a
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-08-12 15:45:18 -0500

    uloaded video thru cloudinary

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,15 +1,27 @@
-const fileInput = document.querySelector("#background-video");
+// const fileInput = document.querySelector("#player");
 
-fileInput.addEventListener("change", async (event) => {
-  const file = event.target.files[0];
+// fileInput.addEventListener("change", async (event) => {
+//   const file = event.target.files[0];
 
-  // Use Cloudinary's upload API to upload the file
-  const response = await fetch("/upload", {
-    method: "POST",
-    body: file,
+//   // Use Cloudinary's upload API to upload the file
+//   const response = await fetch("/upload", {
+//     method: "POST",
+//     body: file,
+//   });
+
+//   if (response.ok) {
+//   } else {
+//   }
+// });
+
+cloudinary.v2.uploader
+  .upload(
+    "https://res.cloudinary.com/dfecvj7s4/video/upload/v1691872048/escape-room-video_hdaybz.mp4",
+    { resource_type: "video", public_id: "video_upload_example" }
+  )
+  .then((data) => {
+    console.log(data.playback_url);
+  })
+  .catch((err) => {
+    console.error(err);
   });
-
-  if (response.ok) {
-  } else {
-  }
-});
