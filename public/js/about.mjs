afd6a683537cf1ba6aa21fd9ac2302b018c75852
moonjunsain <moonjunsain@gmail.com>
2023-08-13 16:54:03 -0700

    added detail page for more information

--- public/js/about.mjs
+++ public/js/about.mjs
@@ -1,28 +1,92 @@
+// importing in three js an helpers
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { PointerLockControls } from 'three/addons/controls/PointerLockControls.js';
 
+// ================== basic setup ==================
+
+// selecting a dom element for canvas
 const canvas = document.getElementById('scene-canvas');
 
+// creating a scene
 const scene = new THREE.Scene();
+
+// setting up a camera (fov, aspect ratio, near(object closer than this will not be rendered), far(how far can the camera see))
 const camera = new THREE.PerspectiveCamera(75, canvas.clientWidth / canvas.clientHeight, 0.1, 2000);
 
-const renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true }); // Use the canvas element
+// setting up the renderer to use canvas element, alpha true means setting up the pixels in model to have transparent background
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, alpha: true }); 
+
+// setting up the size equal to canvas height and width
 renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+
+// setting color of the renderer's background to transparent
 renderer.setClearColor(0x000000, 0);
 
-// const controls = new OrbitControls(camera, renderer.domElement);
-// controls.update();
 
+
+
+// ================== setting the light ==================
+
+// setting up the ambientlight (brightening object) (color, intensity)
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.75);
+scene.add(ambientLight);
+
+// setting up the directional light (color, intensity)
+const directionalLight = new THREE.DirectionalLight(0xffb380, 3); // White directional light
+directionalLight.position.set(1, 1, 1); // Set the direction of the light
+scene.add(directionalLight);
+
+
+
+// ================== importing a 3d model ==================
+
+const loader = new GLTFLoader();
+
+let jungleSetting
+
+// imports (file path, callback)
+loader.load( 'jungle_environment.glb', function ( gltf ) {
+
+	// setting the model to the variable
+	jungleSetting = gltf
+
+	// adding it to our scene
+	scene.add( gltf.scene );
+
+	// positioning the object
+	jungleSetting.scene.position.copy(camera.position);
+	jungleSetting.scene.position.y -= 500
+	jungleSetting.scene.position.x += 460
+	jungleSetting.scene.position.z -= 200
+	jungleSetting.scene.rotation.y = Math.PI / -4;
+		
+} );
+
+// setting camera position
+camera.position.z = 20
+camera.position.y = 5
+
+
+
+// ============================== setting controls ==============================
+
+// adding controls to camera (moving camera with mouse)
 const controls = new PointerLockControls(camera, renderer.domElement)
-const firstKey = true
+let firstKey = true
 
 canvas.addEventListener('click', function(e){
+	// without this event listener, you can't let your user move around the camera
 	controls.lock()
+
+	// zoom into the door when the user presses any key
 	document.addEventListener('keydown', function(e){
+	
+		// only take them to the door the first time they press on a key
 		if(firstKey){
 			firstKey = false
+
+			// setting time interval
 			const interval = setInterval(() => {
 				if(camera.position.z > -280){
 					camera.position.z -= 1
@@ -38,6 +102,7 @@ canvas.addEventListener('click', function(e){
 
 				if(camera.position.z < -830){
 					clearInterval(interval)
+					// TODO: Redirect the user to seperate page with treasure
 				}
 			}, 10);
 		}
@@ -45,56 +110,8 @@ canvas.addEventListener('click', function(e){
 	
 })
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.75); // White light with intensity
-scene.add(ambientLight);
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 3); // White directional light
-directionalLight.position.set(1, 1, 1); // Set the direction of the light
-
-
-scene.add(directionalLight);
-
-const loader = new GLTFLoader();
-
-// let chest
-
-// loader.load( 'treasure_chest.glb', function ( gltf ) {
-
-// 	chest = gltf
-
-// 	scene.add( gltf.scene );
-
-// 	const pointLight = new THREE.PointLight(0x00ff00, 70, 100); 
-//     pointLight.position.set(1, 10, 1); // Set light's position to match chest's position
-//     scene.add(pointLight);
-	
-// } );
-
-let jungleSetting
-
-loader.load( 'jungle_environment.glb', function ( gltf ) {
-
-	jungleSetting = gltf
-
-	scene.add( gltf.scene );
-	jungleSetting.scene.position.copy(camera.position);
-	jungleSetting.scene.position.y -= 500
-	jungleSetting.scene.position.x += 460
-	jungleSetting.scene.position.z -= 200
-	jungleSetting.scene.rotation.y = Math.PI / -4;
-	
-	
-} );
-
-camera.position.z = 20
-camera.position.y = 5
-
-
-
 function animate() {
-	// if(chest){
-	// 	chest.scene.rotation.y += 0.001
-	// }
+	
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
