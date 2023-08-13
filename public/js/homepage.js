839580a74478a2791558754a8b92636628ded5d4
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-08-12 20:49:52 -0500

    mobile drop down nav bar manu

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -9,3 +9,13 @@ cloudinary.v2.uploader
   .catch((err) => {
     console.error(err);
   });
+
+// mobile menu icon and dropdown elements
+const mobileMenuIcon = document.getElementById("mobileMenuIcon");
+const mobileMenuDropdown = document.getElementById("mobileMenuDropdown");
+
+// event listener to the mobile menu icon
+mobileMenuIcon.addEventListener("click", () => {
+  // toggle the 'clicked' class on the icon
+  mobileMenuIcon.classList.toggle("clicked");
+});
