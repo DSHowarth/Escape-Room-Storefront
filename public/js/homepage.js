2d4bb0e98aae094a5cb39816dfa24198dec180c8
elenagurchinskaia <136741210+elenagurchinskaia@users.noreply.github.com>
2023-08-12 20:15:03 -0500

    fixed video autoplay

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,7 +1,7 @@
 cloudinary.v2.uploader
   .upload(
     "https://res.cloudinary.com/dfecvj7s4/video/upload/v1691872048/escape-room-video_hdaybz.mp4",
-    { resource_type: "video", public_id: "video_upload_example" }
+    { resource_type: "video", public_id: "escape-room-video_hdaybz" }
   )
   .then((data) => {
     console.log(data.playback_url);
