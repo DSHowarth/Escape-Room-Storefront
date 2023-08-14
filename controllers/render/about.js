868d1f435eb28060452935c0aa0e711858b99704
moonjunsain <moonjunsain@gmail.com>
2023-08-13 17:34:26 -0700

    fixed the typo

--- controllers/render/about.js
+++ controllers/render/about.js
@@ -2,7 +2,9 @@ const router = require('express').Router()
 
 router.get('/', async (req, res)=>{
     try{
-        res.render('about')
+        res.render("about", {
+            loggedIn: req.session.loggedIn,
+          });
 
     }catch(error){
         res.status(500).json(error)
